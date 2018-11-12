05a08cb61b6d1c2c0ca0188224f33f0573b6462f
Your Name <you@example.com>
2018-11-12 10:07:05 +0800

    第二次

--- git-test/src/cn/itcast/git/HelloWorld.java
+++ git-test/src/cn/itcast/git/HelloWorld.java
@@ -7,5 +7,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("第一次");
+        System.out.println("第一次");
+        System.out.println("第一次");
     }
 }
